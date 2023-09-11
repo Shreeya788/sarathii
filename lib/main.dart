17ce2c000224f4d422aa4fcab609214e80b79b47
Shreeya Shrestha <shresthashreeya788@gmail.com>
2023-09-11 11:26:15 +0545

    added files

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+
       ),
       home:  SplashScreen(),
     );
